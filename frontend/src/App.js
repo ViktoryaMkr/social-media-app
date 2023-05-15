5ac924bb78202df326299d8dd6cd3c58397c89b7
Viktorya <viktorya@shooga.ca>
2023-05-14 20:46:04 -0400

    start creating and styling home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css"
+import HomePage from "./pages/home/HomePage";
+
+
+
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
+        <div className="blur " style={{top: '-13%', right: '0'}}></div>
+        <div className="blur " style={{top: '36%', left: '-8rem'}}></div>
+        <HomePage/>
     </div>
   );
 }
---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomePage.css'
+const HomePage = () => {
+  return (
+    <div className='home'> 
+    <div className='profile-side'>Profile</div>
+    <div className='post-side'>Posts</div>
+    <div className='right-side'>Right side</div>
+    </div>
+  )
+}
+
+export default HomePage
