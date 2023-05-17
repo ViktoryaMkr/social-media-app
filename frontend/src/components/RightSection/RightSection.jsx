7fec489581f0e0e7ae1c217af07b356b54b0ec71
Viktorya <viktorya@shooga.ca>
2023-05-17 18:41:17 -0400

    importing menu icons in home page

--- frontend/src/components/RightSection/RightSection.jsx
+++ frontend/src/components/RightSection/RightSection.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import "./RightSection"
-
-
+import Home from "../../img/home.png";
+import Notif from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
 
 
 const RightSection = () => {
   return (
-    <div>RightSection</div>
+    <div className='right-section'>
+        <div className='navIcons'>
+            <img src={Home} alt="" />
+            <UilSetting/>
+            <img src={Notif} alt="" />
+            <img src={Comment} alt="" />
+
+        </div>
+        
+    </div>
   )
-}
+} 
 
 export default RightSection
