1ea69be99a35c8d0dc36d142e7a950c6a4214773
Viktorya <viktorya@shooga.ca>
2023-05-17 19:09:55 -0400

    Create trending card and style nav icons

--- frontend/src/components/RightSection/RightSection.jsx
+++ frontend/src/components/RightSection/RightSection.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import "./RightSection"
+import "./RightSection.css"
 import Home from "../../img/home.png";
 import Notif from "../../img/noti.png";
 import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
-
+import TrendCard from '../TrendCard/TrendCard';
 
 const RightSection = () => {
   return (
     <div className='right-section'>
-        <div className='navIcons'>
+        <div className='nav-icons'>
             <img src={Home} alt="" />
             <UilSetting/>
             <img src={Notif} alt="" />
             <img src={Comment} alt="" />
+
+            <TrendCard/>
 
         </div>
         
---
+++ frontend/src/components/TrendCard/TrendCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TrendCard.css'
+
+
+const TrendCard = () => {
+  return (
+    <div>TrendCard</div>
+  )
+}
+
+export default TrendCard
