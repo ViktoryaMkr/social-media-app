5b451283f0a0a58ae371c41ae82c7a82f67e4411
Viktorya <viktorya@shooga.ca>
2023-05-19 02:26:10 -0400

    Style left side auth page

--- frontend/src/pages/Auth/AuthPage.jsx
+++ frontend/src/pages/Auth/AuthPage.jsx
@@ -4,17 +4,15 @@ import Logo from '../../img/logo1.png'
 
 const AuthPage = () => {
   return (
-    <div className='Auth'>
+    <div className='auth'>
       <div className='auth-left-section'>
         <img src={Logo} alt="" />
         <div className='app-name'>
           <h1>BuzzWave Network</h1>
           <h6>Making Waves, Creating Buzz, Connecting You</h6>
-
         </div>
-
       </div>
-        
+        <h1>Form</h1>
     </div>
   )
 }
