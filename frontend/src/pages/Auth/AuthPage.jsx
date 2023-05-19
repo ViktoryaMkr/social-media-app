d26da2636dfe1baf35d36d872477f008f8cd0950
Viktorya <viktorya@shooga.ca>
2023-05-19 01:52:45 -0400

    Left side content of the auth page UI

--- frontend/src/pages/Auth/AuthPage.jsx
+++ frontend/src/pages/Auth/AuthPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './AuthPage.css'
-
-
+import Logo from '../../img/logo1.png'
 
 const AuthPage = () => {
   return (
-    <div>AuthPage</div>
+    <div className='Auth'>
+      <div className='auth-left-section'>
+        <img src={Logo} alt="" />
+        <div className='app-name'>
+          <h1>BuzzWave Network</h1>
+          <h6>Making Waves, Creating Buzz, Connecting You</h6>
+
+        </div>
+
+      </div>
+        
+    </div>
   )
 }
 
