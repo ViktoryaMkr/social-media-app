a215b94376ac9b66fd9301906d9155b42a141b46
Viktorya <viktorya@shooga.ca>
2023-05-19 11:37:09 -0400

    add form inputs with react hook form for Sign up

--- frontend/src/pages/Auth/AuthPage.jsx
+++ frontend/src/pages/Auth/AuthPage.jsx
@@ -1,6 +1,51 @@
 import React from 'react'
 import './AuthPage.css'
 import Logo from '../../img/logo1.png'
+import { useForm } from 'react-hook-form';
+
+
+
+
+function SignUp(){
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+
+  return(
+    <div>
+      <form className='info-form' onSubmit={(data) => console.log(data)}>
+        <h3>Sign up</h3>
+        <div>
+          <input {...register('firstName')} 
+                type="text" 
+                placeholder='First Name'
+                className='info-input'
+                name='firstName'
+                />
+          <input {...register('lastName')} 
+                type="text" 
+                placeholder='Last Name'
+                className='info-input'
+                name='lastName'
+                />
+        </div>
+
+        <div>
+          <input className="info-input" type="text" name="username" id="" placeholder='Username' {...register('userName')}/>
+        </div>
+        <div>
+          <input className="info-input" type="text" name="password" id="" placeholder='Password' {...register('password')}/>
+        </div>
+        <div>
+          <input className="info-input" type="text" name="confirmPassword" id="" placeholder='Confirm Password' {...register('confirmPassword')}/>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 const AuthPage = () => {
   return (
@@ -12,7 +57,7 @@ const AuthPage = () => {
           <h6>Making Waves, Creating Buzz, Connecting You</h6>
         </div>
       </div>
-        <h1>Form</h1>
+        <SignUp/>
     </div>
   )
 }
