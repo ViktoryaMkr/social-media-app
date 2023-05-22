2158929e7fea4ffadd8fc1363343bae64aadee95
Viktorya <viktorya@shooga.ca>
2023-05-22 17:17:39 -0400

    Create delete post method in controller

--- backend/Controllers/PostController.js
+++ backend/Controllers/PostController.js
@@ -36,10 +36,30 @@ export const updatePost = async(req, res) => {
         var post = await PostModel.findById(id);
         
         if (post.userId === userId || userAdminStatus) {
-            await post.updateOne( {$set : req.body} )
-            res.status(200).json("Post updated")
+            await post.updateOne( {$set : req.body} );
+            res.status(200).json("Post updated");
         }else {
             res.status(403).json("Action denied. You have no permission to update this post");
+        }
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+export const deletePost = async(req, res) => {
+    const id = req.params.id;
+    const { userId, userAdminStatus } = req.body;
+
+    try {
+
+        var post = await PostModel.findById(id);
+        
+        if (post.userId === userId || userAdminStatus) {
+            await post.deleteOne();
+            res.status(200).json("Post deleted");
+        }else {
+            res.status(403).json("Action denied. You have no permission to delete this post");
         }
 
     } catch (error) {
