2a7e58b93cdd33a06c26b08c8c7796e1cf3ba5e7
Viktorya <viktorya@shooga.ca>
2023-05-24 13:46:43 -0400

    Add JWT auth for registering user

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,20 +1,34 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+
 
 // For new user
 export const registerUser = async(req, res)=> {
-    const {username, password, firstName, lastName} = req.body;
-
     
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    
+    req.body.password = hashPassword;
+    console.log(req.body.password);
+    const newUser = new UserModel(req.body);
 
-
-    const newUser = new UserModel({username, password : hashPassword, firstName, lastName});
+    const { username } = req.body;
 
     try {
-        await newUser.save()
-        res.status(200).json(newUser)
+
+        const existingUser = await UserModel.findOne({username});
+        if(existingUser){
+            return res.status(400).json("Username already taken");
+        }
+
+        const newRegisteredUser = await newUser.save()
+        const token = jwt.sign({
+            username: newRegisteredUser.username,
+            id: newRegisteredUser._id
+        }, process.env.JWT_PKEY , {expiresIn: '5000'})
+        res.status(200).json({newRegisteredUser, token})
+
     } catch (error) {
         res.status(500).json({message:error.message})
     }
